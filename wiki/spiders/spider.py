37c96e8bb57fd7fd6ee681d9a30293f5424f8d22
Javier Junquera Sánchez <javier.junquera.sanchez@gmail.com>
2017-03-19 12:38:30 +0100

    Minor changeMinor changess

--- wiki/spiders/spider.py
+++ wiki/spiders/spider.py
@@ -9,10 +9,13 @@ class SpiderSpider(scrapy.Spider):
 		start_urls = (
 			'https://es.wikipedia.org/wiki/Parque_de_la_Ciudadela',
 		)
-		
+
 		def parse(self, response):	
 			title = response.css('#firstHeading::text').extract_first()
-			for url in response.css('a::attr(href)').extract():
-					yield scrapy.Request(response.urljoin(url), callback=self.parse)
-			pass
+			url = response.url
+			referer = response.request.headers.get('Referer', None)
+
+			for nextUrl in response.css('#mw-content-text a::attr(href)').extract():
+				yield scrapy.Request(response.urljoin(nextUrl), callback=self.parse)
+
 			yield Article(title=title, url=response.url, referer=response.request.headers.get('Referer', None))
